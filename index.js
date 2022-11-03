cdfc38e3270f258284d64cd264d5f11880f38e8a
shashank-iter <pandeyshashank812@gmail.com>
2022-11-04 02:49:49 +0530

    modal added with bootstrap and jQuery

--- index.js
+++ index.js
@@ -1,39 +1,102 @@
-let stacksize=0;
+let stacksize = 0;
 let count = -1;
+let stackStop = 0;
 
-function push(){
+
+function push() {
+     
+    //console.log(modal);
+    if (stacksize < 5) {
+        // modal.style.display = "block";
+        $(document).ready(function(){
+            // Show the Modal on load
+            $("#myModal").modal("show");
+            let textModal=document.getElementById("modal-text");
+            textModal.textContent="Please set the stack size first.";
+            // Hide the Modal
+            $("#close").click(function(){
+              $("#myModal").modal("hide");
+            });
+          });
+        return;
+    }
+    if (count == stacksize - 1) {
+        $(document).ready(function(){
+            // Show the Modal on load
+            $("#myModal").modal("show");
+            let textModal=document.getElementById("modal-text");
+            textModal.textContent="Stack Overflow, can't push elements.";
+            // Hide the Modal
+            $("#close").click(function(){
+              $("#myModal").modal("hide");
+            });
+          });
+        return;
+        // alert("Stack Overflow, can't push");
+        
+    }
     count++;
-    if (stacksize<5){
-        alert("Please set size of the Stack.")
-        return;
-    }
-    if (count>stacksize){
-        alert("Stack Overflow, can't push")
-        return;
-    }
-let x = document.getElementById("push-val").value;
-console.log(x);
-let y = document.getElementById("tab");
-y.innerHTML=`<tr><td id="push${count}">${x}</td></tr>`+y.innerHTML;
+    let x = document.getElementById("push-val").value;
+    console.log(x);
+    let y = document.getElementById(`push${count}`);
+
+    y.innerText = `${x}`;
 }
- function pop(){
-    if (stacksize<5){
-        alert("Please set size of the Stack.")
+
+
+function pop() {
+    if (stacksize < 5) {
+        $(document).ready(function(){
+            // Show the Modal on load
+            $("#myModal").modal("show");
+            let textModal=document.getElementById("modal-text");
+            textModal.textContent="Please set size of the Stack.";
+            // Hide the Modal
+            $("#close").click(function(){
+              $("#myModal").modal("hide");
+            });
+          });
         return;
+       // alert("Please set size of the Stack.");
+        //return;
     }
     let a = document.getElementById(`push${count}`);
-    if (count<0){
-        alert("Stack Underflow: Stack is Empty, cannot POP any element.")
+    if (count < 0) {
+        $(document).ready(function(){
+            // Show the Modal on load
+            $("#myModal").modal("show");
+            let textModal=document.getElementById("modal-text");
+            textModal.textContent="Stack Underflow: Stack is Empty, cannot POP any element.";
+            // Hide the Modal
+            $("#close").click(function(){
+              $("#myModal").modal("hide");
+            });
+          });
         return;
+        // alert("Stack Underflow: Stack is Empty, cannot POP any element.");
+        
     }
     count--;
-    
-    a.remove();
- }
- function setStackSize(){
-    let a = document.getElementById("stacksize").value
-    stacksize=a;
- }
- function reset(){
+
+    a.textContent="Empty";
+}
+
+
+function setStackSize() {
+    stackStop++;
+    if (stackStop > 0) {
+        let y = document.getElementById("tab");
+        y.innerHTML = "";
+    }
+    let a = document.getElementById("stacksize").value;
+    stacksize = a;
+    let y = document.getElementById("tab");
+    for (let i = 0; i < stacksize; i++) {
+        y.innerHTML = `<tr><td id="push${i}">Empty</td></tr>` + y.innerHTML;
+    }
+}
+
+
+function reset() {
     window.location.reload();
- }
+}
